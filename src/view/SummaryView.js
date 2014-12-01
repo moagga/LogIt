9dad8a4fa61c5a8a35de3eb5e1961cc5b07883c4
Mohit Aggarwal <agg.mohit@gmail.com>
2014-12-01 20:45:16 +0530

    CSS & Next/Prev
    1. HTML
    2. Bootstrap CSS added.
    3. Next prev button implemented.

--- src/view/SummaryView.js
+++ src/view/SummaryView.js
@@ -1,31 +1,42 @@
 qLog.SummaryView = Backbone.View.extend({
 
-	el:  '#results',
+	el:  '.weekView',
 	startDate: null,
+	
+	events: {
+		'click button.prev': 'prevWeek',
+		'click button.next': 'nextWeek',
+	},
 	
 	initialize: function (options) {
 		this.listenTo(qLog.Tasks, 'reset', this.renderAll);
-		this.daySummary = this.$('.summary');
 		this.startDate = Date.today().moveToDayOfWeek(1, -1);
+		this.title = this.$('.controls h3');
 	},
 
 	renderAll: function(models){
 		$('#results').html('');
-		this.daySummary.html('');
+		var h = '';
 		var d = this.startDate.clone();
+		h += d.toString('MMM dd, yyyy');
 		for (var i = 0; i < 7; i++){
 			var tasks = models.day(d);
 			this._renderDay(d, tasks);
 			d.addDays(1);
 		}
+		h += " - ";
+		h +=  d.addDays(-1).toString('MMM dd, yyyy');
+		this.title.html(h);
 	},
 	
 	nextWeek: function(){
-	
+		this.startDate.addDays(7);
+		this.renderAll(qLog.Tasks);
 	},
 	
 	prevWeek: function(){
-	
+		this.startDate.addDays(-7);
+		this.renderAll(qLog.Tasks);
 	},
 	
 	_renderDay: function(date, tasks){
